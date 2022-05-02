d810098d1ff7b68161390f3f3f1e2dbf3afb6ce3
jeehmilena <jessicamilenacorrea@gmail.com>
2022-05-02 18:41:49 -0300

    Adição do binding no adapter

--- app/src/main/java/com/jessica/yourfavoritemovies/adapter/MovieAdapter.kt
+++ app/src/main/java/com/jessica/yourfavoritemovies/adapter/MovieAdapter.kt
@@ -1,16 +1,14 @@
 package com.jessica.yourfavoritemovies.adapter
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.core.content.res.ResourcesCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.jessica.yourfavoritemovies.util.Constants.BASE_IMAGE_URL
 import com.jessica.yourfavoritemovies.R
+import com.jessica.yourfavoritemovies.databinding.ItemRecyclerViewBinding
 import com.jessica.yourfavoritemovies.model.Result
+import com.jessica.yourfavoritemovies.util.Constants.BASE_IMAGE_URL
 
 class MovieAdapter(
     var listMovie: MutableList<Result>,
@@ -18,18 +16,18 @@ class MovieAdapter(
 ) : RecyclerView.Adapter<MovieAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.item_recycler_view, parent, false)
+        val binding =
+            ItemRecyclerViewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ViewHolder(
-            view
+            binding
         )
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.onBind(listMovie[position])
-        holder.favorite.setOnClickListener {
+        holder.binding.ivFavorite.setOnClickListener {
             listMovie[position].favorited = true
-            holder.favorite.setImageDrawable(
+            holder.binding.ivFavorite.setImageDrawable(
                 ResourcesCompat.getDrawable(
                     holder.itemView.resources,
                     R.drawable.ic_favorite,
@@ -52,35 +50,13 @@ class MovieAdapter(
         notifyDataSetChanged()
     }
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        private val title = itemView.findViewById<TextView>(R.id.tv_title)
-        private val image = itemView.findViewById<ImageView>(R.id.iv_movie)
-        val favorite = itemView.findViewById<ImageView>(R.id.iv_favorite)
-
+    class ViewHolder(val binding: ItemRecyclerViewBinding) : RecyclerView.ViewHolder(binding.root) {
         fun onBind(result: Result) {
-            title.text = result.title
-
-            if (result.favorited){
-                favorite.setImageDrawable(
-                    ResourcesCompat.getDrawable(
-                        itemView.resources,
-                        R.drawable.ic_favorite,
-                        null
-                    )
-                )
-            }else{
-                favorite.setImageDrawable(
-                    ResourcesCompat.getDrawable(
-                        itemView.resources,
-                        R.drawable.ic_favorite_yellow,
-                        null
-                    )
-                )
-            }
+            binding.tvTitle.text = result.title
 
             Glide.with(itemView.context).load("$BASE_IMAGE_URL${result.posterPath}")
                 .placeholder(R.mipmap.ic_movie)
-                .into(image)
+                .into(binding.ivMovie)
         }
     }
 }
